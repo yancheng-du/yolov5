93faffb1065e97c9899353c86c571b662fe559c5
Yancheng Du <duyc88@gmail.com>
2022-05-01 22:22:42 -0500

    added custom model

--- screen_cap.py
+++ screen_cap.py
@@ -12,6 +12,7 @@ from utils.torch_utils import select_device, time_sync
 from torchvision import transforms
 
 import win32api, win32con
+import pyautogui
 import mss
 
 
@@ -21,10 +22,10 @@ def cal_time(last_time, time_to_cal,n):
 	last_time = current_time
 	return ret,last_time
 
-def inblock(mouse_x, mouse_y, block, r=0.5):
+def inblock(mouse_x, mouse_y, block, r=1):
 	#print(block[0:4])
 	#print(mouse_x,mouse_y)
-	return 0.9*block[0]+0.1*block[2]< mouse_x < 0.9*block[2]+0.1*block[0] and block[1] < mouse_y < 0.8*(block[3]-block[1])+block[1]
+	return 0.9*block[0]+0.1*block[2]< mouse_x < 0.9*block[2]+0.1*block[0] and block[1] < mouse_y < r*(block[3]-block[1])+block[1]
 
 def main():
 	
@@ -44,11 +45,11 @@ def main():
 		device = 'cpu'
 	
 	#model parameters
-	weights = 'yolov5n.pt'
+	weights = 'yolov5s.pt'
 	dnn = False
 	data = 'data/coco128.yaml'
 	half = False
-	model = DetectMultiBackend(weights, device=device, dnn=dnn, data=data)
+	model = DetectMultiBackend(weights, device=device, dnn=dnn, data=None)
 	print("yolo model created")
 	
 	# prediction parameters
@@ -57,7 +58,7 @@ def main():
 	max_det = 50
 	classes = 0	# only detecting people
 	agnostic_nms=False
-	move = False
+	move = True
 	
 	move_speed = 0.1
 	
@@ -97,7 +98,7 @@ def main():
 			detect = False
 			for objects in pred[0]:
 				center_x = int(0.5*(objects[0]+objects[2]))
-				center_y = int(0.8*objects[1]+0.2*objects[3])
+				center_y = int(0.5*objects[1]+0.5*objects[3])
 				dist_from_mouse = np.linalg.norm([center_x-crop_center[1], center_y-crop_center[0]])
 				if (dist_from_mouse<min_dist):
 					min_dist = dist_from_mouse
@@ -106,12 +107,16 @@ def main():
 			
 			if move:
 				if detect:
-						move_speed = np.exp(-0.0001*min_dist)
-						dx = int(0.50*move_speed*(min_loc[0]-crop_center[1])+0.50*dx)
-						# target_x = max(30,min(-30,target_x))
-						dy = int(0.50*move_speed*(min_loc[1]-crop_center[0])+0.50*dy)
-						# target_y = max(30, min(-30, target_y))
-						win32api.mouse_event(win32con.MOUSEEVENTF_MOVE, dx, dy, 0, 0)
+					move_speed = 1
+					dx = np.clip(int(0.50*move_speed*(min_loc[0]-crop_center[1])+0.50*dy), -90, 90)
+					# target_x = max(30,min(-30,target_x))
+					dy = np.clip(int(0.50*move_speed*(min_loc[1]-crop_center[0])+0.50*dy), -30, 30)
+					# target_y = max(30, min(-30, target_y))
+					if(np.abs(min_loc[0]-crop_center[1]) < 5):
+						dx = 0
+					if(np.abs(min_loc[1]-crop_center[0]) < 5):
+						dy = 0
+					win32api.mouse_event(win32con.MOUSEEVENTF_MOVE, dx, dy, 0, 0)
 			else:
 				for object in pred[0]:
 					if inblock(crop_center[1],crop_center[0],object):
@@ -125,6 +130,9 @@ def main():
 			cv2.imshow("OpenCV/Numpy normal", img_out)
 			
 			mouse_move_time,last_time  = cal_time(last_time, mouse_move_time, n)
+			
+			if cv2.waitKey(25)&0xFF==ord("t"):
+				move = not move
 			
 			if cv2.waitKey(25)&0xFF==ord("q"):
 				cv2.destroyAllWindows()
